ae6b702017246eac94e7ebd6de55cddd90f3c88b
hyeon11ok <hyeonsusin05@gmail.com>
2025-04-22 10:50:10 +0900

    Add : Scene.cs 파일 생성
    - 각 화면 출력 객체에 상속할 최상위 추상 클래스 생성
    - 장면 출력을 위한 추상 메서드 선언
    - 화면 출력 메서드를 스택 구조로 관리하기 위해 Action 타입의 변수 생성 및 메서드 할당

---
+++ TextRPG/TextRPG/Scene/Scene.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.Scene
+{
+    internal abstract class Scene
+    {
+        protected string sceneName;
+        protected string sceneDescription;
+
+        /// <summary>
+        /// 장면을 보여주는 메소드
+        /// 장면을 스택 구조를 활용해 구현하기 위해 Action으로 정의
+        /// </summary>
+        public Action Show { get; }
+
+        public Scene(string sceneName, string sceneDescription)
+        {
+            this.sceneName = sceneName;
+            this.sceneDescription = sceneDescription;
+            Show = ShowScene;
+        }
+
+        /// <summary>
+        /// 장면을 보여주는 메소드
+        /// </summary>
+        public abstract void ShowScene();
+    }
+}
