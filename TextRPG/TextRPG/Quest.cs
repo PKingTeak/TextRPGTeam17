3fd6068c3d8e860d491d559fcdedf9fcd4323344
nwoorang <89909664+nwoorang@users.noreply.github.com>
2025-04-22 11:02:12 +0900

    Add :Quest클래스 추가, Feat :퀘스트 UI설정
    1.QuestManager에 Quest클래스의 묶음인 리스트를 생성하여 관리
    2.퀘스트 UI창 기본창 구현

---
+++ TextRPG/TextRPG/Quest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    class Quest
+    {
+        public int QuestID { get;private set; } //퀘스트 ID
+        string questTitle; //퀘스트 제목
+        string questContent; //퀘스트 내용
+        string questCondition; //퀘스트 조건
+        string questReward; //퀘스트 보상
+        int minCount; //수집 수
+        int maxCount; //목표 조건 수
+        public  Quest(int id=0 ,string title = "", string content = "", string condition = "", string reward = "") //퀘스트 화면
+        {
+            QuestID = id;
+            questCondition = condition;
+            questContent = content;
+            questTitle = title;
+            questReward = reward;
+        }
+
+        public void QuestInfo() //퀘스트 정보
+        {
+            Console.WriteLine("Quest!!");
+            Console.WriteLine(questTitle);
+            Console.WriteLine(questContent);
+            Console.WriteLine(questCondition);
+            Console.WriteLine($"{minCount}/{maxCount}");
+            Console.WriteLine(questReward);
+        }
+
+        public void GetCount(int minCount, int maxCount) //퀘스트 수집 수
+        {
+            this.minCount = minCount;
+            this.maxCount = maxCount;
+        }
+    }
+}
--- TextRPG/TextRPG/QuestManager.cs
+++ TextRPG/TextRPG/QuestManager.cs
@@ -1,23 +1,41 @@
 ﻿using System;
+using System.Diagnostics;
 using TextRPG;
 
 public class QuestManager
 {
-    int killCount = 0; //처치한 몬스터 수
+    List<Quest> questList = new List<Quest>(); //퀘스트 리스트
+
+    int minCount = 0; //처치한 몬스터 수
+    int maxCount = 5; //처치해야 할 몬스터 수
     public void Subscribe(Monster monster)//몬스터가 구독해서 HandleMonsterKilled()를 쓸 수 있게 해줌
     {
-       
         monster.OnMonsterKilled += HandleMonsterKilled;
     }
     private void HandleMonsterKilled() //몬스터에게서 OnMonsterKilled?.Invoke();될 때 마다 호출
     {
-        this.killCount ++; //처치한 몬스터 수 증가
-        if(killCount >= 10) //처치한 몬스터 수가 10 이상이면
+        this.minCount ++; //처치한 몬스터 수 증가
+        if(minCount >=maxCount) //처치한 몬스터 수가 5 이상이면
         {
-            Console.WriteLine($"[퀘스트 업데이트] 몬스터 10마리 처치 완료!");
+            Console.WriteLine($"[퀘스트 업데이트] 몬스터 5마리 처치 완료!");
         }
     }
 
+    public void setQuest(int id=0,string title = "", string content = "", string condition = "", string reward = "") //퀘스트 화면
+    {
+        Quest quest = new Quest(id,title, content, condition, reward);
+        questList.Add(quest);
+    }
 
-
+    public void getQuest(int i) //퀘스트 정보
+    {
+        foreach (var quest in questList)
+        {
+            if(quest.QuestID == i)
+            {
+                quest.GetCount(minCount, maxCount); //퀘스트 수집 수
+                quest.QuestInfo();
+            }
+        }
+    }
 }
