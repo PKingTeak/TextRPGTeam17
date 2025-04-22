81b047c17ae6a279f2617cb72baf6d38ac4dad1f
hyeon11ok <hyeonsusin05@gmail.com>
2025-04-22 10:25:48 +0900

    Feat : 선택지 출력 및 입력 기능 구현
    - Scene 폴더 생성
    - Scene 폴더 하위에 입력을 관리할 InputHandler.cs 파일 생성
    - 출력할 선택지 문자열과 입력 대기 문자열을 받아와 선택지 출력 후 유저의 입력 대기
    - 선택지 범위를 받아와 유저의 입력과 비교하여 조건에 맞으면 입력한 값을, 조건에 맞지 않으면 -1을 반환

---
+++ TextRPG/TextRPG/Scene/InputHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TextRPG.Scene
+{
+    internal static class InputHandler
+    {
+        /// <summary>
+        /// 사용자에게 선택지를 주고, 그에 대한 입력을 받는 메소드
+        /// </summary>
+        /// <param name="min">선택지 범위 최소</param>
+        /// <param name="max">선택지 범위 최대</param>
+        /// <param name="optionTxt">출력할 선택지 텍스트</param>
+        /// <param name="comment">입력 대기 텍스트</param>
+        /// <returns>입력한 값이 숫자가 아니거나 범위를 벗어나면 -1, 조건에 맞으면 입력한 값 반환</returns>
+        public static int ChooseAction(int min, int max, string optionTxt, string comment)
+        {
+            int choice = -1;
+
+            // 선택지 출력후 콘솔 초기화
+            Console.WriteLine(optionTxt);
+            Console.WriteLine();
+            Console.WriteLine(comment);
+            Console.Write(">> ");
+            string input = Console.ReadLine();
+            Console.Clear();
+
+            int inputNum;
+            bool isInt = int.TryParse(input, out inputNum);
+
+            if(isInt && min <= inputNum && inputNum <= max)
+            {
+                return inputNum;
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다.");
+                Console.WriteLine();
+                return -1;
+            }
+        }
+    }
+}
