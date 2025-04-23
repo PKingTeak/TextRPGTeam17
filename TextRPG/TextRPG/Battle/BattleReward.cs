9f80a470359e71077e2b4cb84b43ef3bedd1e501
Jangminez <111687963+Jangminez@users.noreply.github.com>
2025-04-23 19:44:46 +0900

    Refactor: Player SetJob 수정

--- TextRPG/TextRPG/Battle/BattleReward.cs
+++ TextRPG/TextRPG/Battle/BattleReward.cs
@@ -1,20 +1,21 @@
+public struct RewardBundle
+{
+    int RewardExp { get; }
+    int RewardGold { get; }
+
+    // 아이템도 추가 해야함
+
+    // 층수에 따른 보상 설정
+    public RewardBundle(int floor)
+    {
+        RewardExp = 10 * floor;
+        RewardGold = 20 * floor;
+    }
+}
 
 public class BattleReward
 {
-    public struct RewardBundle
-    {
-        int RewardExp { get; }
-        int RewardGold { get; }
 
-        // 아이템도 추가 해야함
-
-        // 층수에 따른 보상 설정
-        public RewardBundle(int floor)
-        {
-            RewardExp = 10 * floor;
-            RewardGold = 20 * floor;
-        }
-    }
 
     public RewardBundle GetReward(int floor)
     {
